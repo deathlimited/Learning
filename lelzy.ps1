8a185296311f38ab4734bb461cffd4e2f2686c14
deathlimited <denis_srk@live.com>
2017-11-17 03:30:59 +0200

    Update lelzy.ps1

--- lelzy.ps1
+++ lelzy.ps1
@@ -1,6 +1,4 @@
-$url = "https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat"
-$outpath = "$PSScriptRoot/lel.bat"
-Invoke-WebRequest -Uri = $url -OutFile $outpath
-
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat, $PSScriptRoot/lel.bat)
 $args = @("Comma","Separated","Arguments")
 Start-Process -Filepath "$PSScriptRoot/lel.bat" -ArgumentList $args
