3bd1d80110c91ed93ea0291706022fcd0d657874
deathlimited <denis_srk@live.com>
2017-11-17 09:05:02 +0200

    Update lelzy.ps1

--- lelzy.ps1
+++ lelzy.ps1
@@ -1,5 +1,5 @@
 $url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'
 $outpath = '$env:Temp\lel.bat'
-Invoke-WebRequest -Uri = $url -OutFile $outpath
+Invoke-WebRequest -Uri = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat' -OutFile $outpath
 $args = @("Comma","Separated","Arguments")
 Start-Process -Filepath "$env:Temp/lel.bat" -ArgumentList $args
