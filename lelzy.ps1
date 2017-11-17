b91bb6d39444768b1588ba38ce62edea3db9aa76
deathlimited <denis_srk@live.com>
2017-11-17 03:33:59 +0200

    Update lelzy.ps1

--- lelzy.ps1
+++ lelzy.ps1
@@ -1,6 +1,6 @@
 $wc = New-Object System.Net.WebClient
 $url ="https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat, $PSScriptRoot/lel.bat"
-$outpath ="$C:\lel.bat";
+$outpath ="C:\lel.bat";
 $wc.DownloadFile($url, $outpath)
 $args = @("Comma","Separated","Arguments")
 Start-Process -Filepath "C:/lel.bat" -ArgumentList $args
